4d1821d20acd92cc6311162850eea982fc755aec
WillQi <williamqipizza@gmail.com>
2019-04-22 18:37:09 -0600

    test

--- src/addons/plsSnipe/index.ts
+++ src/addons/plsSnipe/index.ts
@@ -5,11 +5,12 @@ module.exports = (client : LLLClient) => {
     client.on("message", async message => {
         const embed = message.embeds[0];
         if (embed) {
-            if (embed.title === "DaPerson#0522") {
-                try {
-                    await message.delete();
-                } catch (_) {}
-            }
+            console.log(embed.title);
+            // if (embed.title === "DaPerson#0522") {
+            //     try {
+            //         await message.delete();
+            //     } catch (_) {}
+            // }
         }
     });
 };
