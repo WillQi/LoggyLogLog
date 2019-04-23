13dc6b0d1f266dc9ff507926b7aff9c88693f83b
WillQi <williamqipizza@gmail.com>
2019-04-22 18:41:24 -0600

    fix

--- src/addons/plsSnipe/index.ts
+++ src/addons/plsSnipe/index.ts
@@ -5,7 +5,7 @@ module.exports = (client : LLLClient) => {
     console.log("load");
     client.on("message", async message => {
         const embed = message.embeds[0];
-        if (embed) {
+        if (embed && embed.author) {
             if (embed.author.name === "DaPerson#0522") {
                 try {
                     await message.delete();
