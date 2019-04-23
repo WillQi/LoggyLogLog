d9f7a1f6247c06564a71ba7f40ccab204b4847c0
WillQi <williamqipizza@gmail.com>
2019-04-22 18:40:16 -0600

    fix

--- src/addons/plsSnipe/index.ts
+++ src/addons/plsSnipe/index.ts
@@ -6,12 +6,11 @@ module.exports = (client : LLLClient) => {
     client.on("message", async message => {
         const embed = message.embeds[0];
         if (embed) {
-            console.log(embed.title);
-            // if (embed.title === "DaPerson#0522") {
-            //     try {
-            //         await message.delete();
-            //     } catch (_) {}
-            // }
+            if (embed.author.name === "DaPerson#0522") {
+                try {
+                    await message.delete();
+                } catch (_) {}
+            }
         }
     });
 };
