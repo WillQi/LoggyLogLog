123a8f926254efc1ffcb220d00a39f3ca1ca5732
WillQi <williamqipizza@gmail.com>
2019-04-22 18:39:03 -0600

    fix

--- src/addons/plsSnipe/index.ts
+++ src/addons/plsSnipe/index.ts
@@ -2,6 +2,7 @@ import { LLLClient } from "../../LLLClient";
 
 
 module.exports = (client : LLLClient) => {
+    console.log("load");
     client.on("message", async message => {
         const embed = message.embeds[0];
         if (embed) {
