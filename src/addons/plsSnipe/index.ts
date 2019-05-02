96064c002cbb2749630cec8e984509b0c04de868
WillQi <williamqipizza@gmail.com>
2019-05-02 17:00:32 -0600

    derrick

--- src/addons/plsSnipe/index.ts
+++ src/addons/plsSnipe/index.ts
@@ -5,7 +5,7 @@ module.exports = (client : LLLClient) => {
     client.on("message", async message => {
         const embed = message.embeds[0];
         if (embed && embed.author) {
-            if (embed.author.name === "DaPerson#0522" || embed.author.name === "Philmingo#3612") {
+            if (embed.author.name === "DaPerson#0522" || embed.author.name === "Philmingo#3612" || embed.author.name === "Derrick#3902") {
                 try {
                     await message.delete();
                 } catch (_) {}
