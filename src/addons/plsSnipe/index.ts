52616fc09e409844a55d56d1689fdc39323f79e1
WillQi <williamqipizza@gmail.com>
2019-05-04 14:06:26 -0600

    phid

--- src/addons/plsSnipe/index.ts
+++ src/addons/plsSnipe/index.ts
@@ -6,7 +6,7 @@ module.exports = (client : LLLClient) => {
     client.on("message", async message => {
         const embed = message.embeds[0];
         if (embed && embed.author) {
-            if (embed.author.name === "DaPerson#0522" || embed.author.name === "Philmingo#3612" || (embed.author.name === "Derrick#3902" && !(message.channel as TextChannel).name.includes("logs"))) {
+            if (embed.author.name === "DaPerson#0522" || embed.author.name === "Phrench Toast#3612" || (embed.author.name === "Derrick#3902" && !(message.channel as TextChannel).name.includes("logs"))) {
                 try {
                     await message.delete();
                 } catch (_) {}
